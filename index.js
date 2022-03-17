f1aec815fa47397cd65dd2b1d767e1eb7ec1b2d7
BooKi <qnrl9034@naver.com>
2022-03-18 00:03:28 +0900

    Make login

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const mongoose = require('mongoose')
 const { User } = require("./models/User")
 const bodyParser = require('body-parser')
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 mongoose.connect(config.mongoURI
 ).then( () => console.log('MongoDB Connected'))
@@ -25,6 +27,38 @@ app.post('/signup', (req, res) => {
     if (err) return res.json({ success: false, err})
     return res.status(200).json({
       success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "해당 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch){
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다."
+        })
+      }
+      user.generateToken((err, user) => {
+        if(err) {
+          return res.status(400).send(err)
+        }
+                
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
+      })
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,7 +31,7 @@ const userSchema = mongoose.Schema({
 })
 
 userSchema.pre('save', function(next){
-    var user = this;
+    let user = this;
 
     if (user.isModified('password')){
         bcrypt.genSalt(saltRounds, function (err, salt) {
@@ -41,8 +42,28 @@ userSchema.pre('save', function(next){
                 next()
             })
         })
+    } else {
+        next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    let user = this
+    let token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 

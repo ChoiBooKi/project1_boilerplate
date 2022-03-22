d3d24bb919c1423a5e1f16d8b4473db50b27aaa6
BooKi <qnrl9034@naver.com>
2022-03-23 01:42:07 +0900

    make signup

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { LOGIN_USER } from './types'
+import { SIGNUP_USER } from './types'
 
 export function loginUser(dataToSubmit) {
     const request = axios.post('/api/users/login', dataToSubmit)
@@ -10,3 +11,14 @@ export function loginUser(dataToSubmit) {
         payload: request
     }
 }
+
+export function signupUser(dataToSubmit) {
+    const request = axios.post('/api/users/signup', dataToSubmit)
+    .then(response => response.data )
+
+    return {
+        type: SIGNUP_USER,
+        payload: request
+    }
+}
+
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { signupUser } from '../../../_actions/user_action'
 
-function RegisterPage() {
+
+function RegisterPage(props) {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const  [Email, setEmail] = useState("")
+  const  [Name, setName] = useState("")
+  const  [Password, setPassword] = useState("")
+  const  [ConfirmPassword, setConfirmPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value)
+  }
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value)
+  }
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value)
+  }
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value)
+  }
+
+  // document.addEventListener('DOMContentLoaded', () => {
+  //   const p = document.querySelector('p')
+  //   const input = document.querySelector('#input1')
+  //   console.log(p)
+  //   input.addEventListener('keyup', () => {
+  //     if(Password !== ConfirmPassword){
+  //       alert('뭐하냐')
+  //       p.textContent = '비밀번호가 같지 않습니다'
+        
+  //     }
+  //   })
+  // })
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if(Password !== ConfirmPassword){
+      return alert('비밀번호가 같지않습니다.')
+    }
+
+    let body = {
+      email: Email,
+      name: Name,
+      password: Password
+    }
+
+    dispatch(signupUser(body))
+      .then(response => {
+        if (response.payload.success) {
+          navigate('/LoginPage')
+        } else {
+          alert('Error')
+        }
+    })
+  }
+
   return (
-    <div>
-      RegisterPage
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      width: '100%', height: '100vh'
+    }}>
+      <form style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+
+        <label>Confirm Password</label>
+        <input type="password" value={ConfirmPassword} onChange={onConfirmPasswordHandler} id="input1" />
+        {/* <p>안녕</p> */}
+        <br />
+        <button type="submit">
+          Register
+        </button>
+      </form>
     </div>
   )
 }
